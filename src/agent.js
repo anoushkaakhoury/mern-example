33829a5188f33f0dcaf4485d3840c96d288f0302
anoushkaakhoury <53602706+anoushkaakhoury@users.noreply.github.com>
2021-02-12 14:05:58 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://13.229.157.47:3000/api';
+const API_ROOT = 'http://13.229.157.47:3000/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
