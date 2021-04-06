33199ff07f5c32383baafb5580e94c6943cb12c8
anoushkaakhoury <53602706+anoushkaakhoury@users.noreply.github.com>
2021-04-06 15:13:58 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://vpc-cf.squareops.xyz/api';
+const API_ROOT = 'https://docker.squareops.xyz/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
